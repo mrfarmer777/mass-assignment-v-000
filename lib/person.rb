7cbd3925869fb817066dd05fa06157cb68d82c0c
Matt Farmer <mrfarmer777@gmail.com>
2018-06-01 04:10:51 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,6 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_widthend
 
   def initialize(attrs)
-    attrs.each {|key,value| self.send('#{key}=',value)} #for each attribute, call self.key=value
+    attrs.each {|key,value| self.send('#{'+key+''}=',value)} #for each attribute, call self.key=value
   end
 end
